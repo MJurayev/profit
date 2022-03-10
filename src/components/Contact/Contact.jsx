e4150c6e8aa0bf2a975ee6d2a07d0d9bace01374
MJurayev <jurayevmansurbek667@gmail.com>
2022-03-10 15:25:43 +0500

    initialized Contact Component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Contact.scss"
+const Contact = () => {
+    return (
+        <section className='contact container'>
+            
+        </section>
+    );
+}
+
+export default Contact;
